78efd93765eeabd6d9993f56d9c72dac679a46e8
Andrew Spratley <aps831@yahoo.co.uk>
2026-01-23 08:30:10 +0000

    feat: add increment of version from 0 to 1

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.20@sha256:eddacbc7e24bf8799a4ed3cdcfa50d4b88a323695ad80f317b6629883b2c2a78
 HEALTHCHECK NONE
-RUN apk add git --no-cache
+RUN apk update && apk add bash git --no-cache
 ARG VERSION=6.0.1
 RUN wget -O conventional_commits_next_version.zip https://gitlab.com/DeveloperC/conventional_commits_next_version/-/jobs/artifacts/bin-$VERSION/download?job=release-binary-compiling-x86_64-linux-musl \
     && unzip conventional_commits_next_version.zip \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
-#!/bin/sh -l
+#!/bin/bash
 set +e
 calculation_mode=$1
 from_version=$2
 from_reference=$3
 git status
-if [ "${from_version}" = "0.0.0" ]; then
+message=$(git log -1 --pretty=%B)
+if [[ ${from_version} == "0.0.0" ]]; then
   next_version="0.1.0"
+elif [[ ${from_version} =~ ^"0" && ${message} =~ ^"feat: initial stable release" ]]; then
+  next_version="1.0.0"
 else
   next_version=$(conventional_commits_next_version --calculation-mode "${calculation_mode}" --from-version "${from_version}" --from-reference "${from_reference}")
 fi
